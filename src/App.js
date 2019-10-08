b9cd7b743d4da33093dac4959669fac669bc995f
José Augusto <joseaugustolins@gmail.com>
2019-10-08 20:47:09 -0300

    ajuste

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Liga from './liga';
-import Time from './time';
+
 
 
 function App() {
-    const [time, setTime] = useState([]);
     const [liga, setLiga] = useState([]);
     const [atletas, setAtletas] = useState([]);    
+    const [dadosTime, setDadosTime] = useState([]);    
 
 
+    function pontuacaoAtleta(id){
+      axios.get("https://cors-anywhere.herokuapp.com/https://api.cartolafc.globo.com/atletas/pontuados/").then(res=>{
+        const a = res.data.atletas[id].pontuacao;        
+        console.log("aaaa "+a);
+        return a;
+      }).catch(error => {
+        console.log(error)
+      })
+    }
 
-    function getTime(time) {
-      console.log("timae"+time)
-      axios.get('https://cors-anywhere.herokuapp.com/https://api.cartolafc.globo.com/time/id/'+time+'/22',
-      { headers: { 'X-GLB-Token': '' } }).then(response => {
-        setAtletas(response.data.atletas);
-        setTime(response.data);
+    function getTime(time) {     
+      var atltemp; 
+      var pontoAtleta = 8;
+      axios.get('https://cors-anywhere.herokuapp.com/https://api.cartolafc.globo.com/time/id/'+time,
+      { headers: { 'X-GLB-Token': '1224107e0cf31aca0fbb6473137441744614d4674476b5467656c443964634476466852432d693849697453537358336e3559614939687230435930436e3142426e6762656d737967524a7251586e6a326c79444d63347269494576555435386a2d5135626f773d3d3a303a6a6f73652e6a6f73656175677573746f6c696e73' } }).then(response => {
+        atltemp = response.data.atletas;
+        atltemp.map(atleta=>{
+          axios.get("https://cors-anywhere.herokuapp.com/https://api.cartolafc.globo.com/atletas/pontuados/").then(res=>{
+            pontoAtleta = res.data.atletas[atleta.atleta_id].pontuacao;                        
+          }).catch(error => {
+            console.log(error)
+          })
+          atleta.pontos_num = pontoAtleta;
+        })   
+        
+        setAtletas(atltemp);
+        
+
+        setDadosTime(response.data.time)
+        
         console.log(response.data);
       })
       .catch(error => {
@@ -28,7 +51,7 @@ function App() {
     useEffect( () => {
         
         function loadLiga(){axios.get('https://cors-anywhere.herokuapp.com/https://api.cartolafc.globo.com/auth/liga/soccer-and-beer',
-              { headers: { 'X-GLB-Token': '' } }).then(response => {
+              { headers: { 'X-GLB-Token': '1224107e0cf31aca0fbb6473137441744614d4674476b5467656c443964634476466852432d693849697453537358336e3559614939687230435930436e3142426e6762656d737967524a7251586e6a326c79444d63347269494576555435386a2d5135626f773d3d3a303a6a6f73652e6a6f73656175677573746f6c696e73' } }).then(response => {
                 console.log(response.data)
                 setLiga(response.data.times);
               })
@@ -41,9 +64,8 @@ function App() {
 
   return (
     <div className="App">
-
-        <Liga liga={liga} selecionaTime={getTime}/>        
-        <Time time={time} atletas={atletas}/>
+        
+        <Liga liga={liga} selecionaTime={getTime} atletas={atletas} dadosTime={dadosTime}  />        
         
     </div>
   );
--- src/liga.js
+++ src/liga.js
@@ -1,34 +1,100 @@
 import React from 'react'
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { lightBaseTheme } from 'material-ui/styles';
+import CardHeader from '@material-ui/core/CardHeader';
+import Avatar from '@material-ui/core/Avatar';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import IconButton from '@material-ui/core/IconButton';
+import axios from 'axios'
+
 
 function formataNumero(num){
     return new Intl.NumberFormat('en-GB',{maximumFractionDigits:2}).format(num);
  }
+ 
+let exibe=1;
 
-function Liga({liga, selecionaTime}) {
 
-    const item = (time) => (
-      <tr key={time.time_id}>
-        <td><button onClick={()=>selecionaTime(time.time_id)}>{time.nome}</button></td>
-        <td>{formataNumero(time.pontos["rodada"])}</td>
-        <td>{formataNumero(time.pontos["campeonato"])}</td>
-      </tr>
-    )
 
-    const items = liga.map(i=>(
-        item(i)
-    ))
+function premiacao(posicao){
+
+  if(posicao>=34){
+    return "PAGAR GELADA"
+  }
+
+  switch(posicao) {
+    case 1:
+      return " Premiação R$ 2.000";
+    case 2:
+      return " Premiação R$ 1.000";
+    case 3:
+      return " Premiação R$ 900";  
+    case 4:
+      return " Premiação R$ 600";  
+    case 5:
+      return " Premiação R$ 500";
+    case 7:
+      return " Premiação R$ 400";  
+    case 8:
+      return " Premiação R$ 300";  
+    default:
+      return "";
+  }
+}
+
+
+
+function Liga({liga, atletas, selecionaTime, dadosTime}) {  
+
+  
+
+
+  const listaAtletas = atletas.map(
+    atleta=>{                           
+     return   <li key={atleta.atleta_id}> {atleta.apelido} - {atleta.pontos_num} </li>
+    })
 
     return (
-        <div>
-            <table>
-              <thead>
-                  <tr><th>Nome</th><th>Total</th><th>Mês</th></tr>
-              </thead>
-                <tbody>
-                {items}
-                </tbody>
-              
-            </table>
+      
+        <div>            
+               {liga.map(time=>(
+                  
+                     <Card key={time.time_id} >
+                       <CardActionArea onClick={()=>selecionaTime(time.time_id)} >
+                       <CardHeader
+                          avatar={
+                        <Avatar aria-label="recipe">
+                          {time.ranking["campeonato"]}
+          </Avatar>
+        }        
+         
+        title={time.nome +"  Campeonato: " + formataNumero(time.pontos["campeonato"]) + premiacao(time.ranking["campeonato"])}
+        subheader={"Rodada "+formataNumero(time.pontos["rodada"])}>
+  
+        </CardHeader>
+      </CardActionArea>
+      {dadosTime["time_id"]===time.time_id?
+                     <CardContent>
+                     
+                       <Typography>
+                       {listaAtletas}
+                       </Typography>
+                       
+                     </CardContent>      
+                    :""}
+                   </Card>
+               ))}
+                
+             
+                
+
+
         </div>
     )
 }
